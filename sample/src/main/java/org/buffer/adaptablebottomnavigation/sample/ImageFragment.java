51ebc8d5cbde53c14ff9f61456e057d35cc8fcdf
sNajdan <snajdan0525@gmail.com>
2017-05-07 21:12:38 +0800

    Remove unused R.java import

--- sample/src/main/java/org/buffer/adaptablebottomnavigation/sample/ImageFragment.java
+++ sample/src/main/java/org/buffer/adaptablebottomnavigation/sample/ImageFragment.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-import org.buffer.simplebottomnavigation.sample.R;
 
 public class ImageFragment extends Fragment {
 
